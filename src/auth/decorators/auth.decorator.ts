ec2eb272af13b6c958a0e88427bfa58c10a73ffa
thezxzx <oscarmiguelayalaruelas@gmail.com>
2023-07-09 15:18:29 -0700

    Crear decoradores y guards para los usuarios

---
+++ src/auth/decorators/auth.decorator.ts
@@ -0,0 +1,12 @@
+import { UseGuards, applyDecorators } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UserRoleGuard } from '../guards/user-role.guard';
+import { ValidRoles } from '../interfaces';
+import { RoleProtected } from './role-protected.decorator';
+
+export const Auth = (...roles: ValidRoles[]) => {
+  return applyDecorators(
+    RoleProtected(...roles),
+    UseGuards(AuthGuard(), UserRoleGuard),
+  );
+};
--- src/auth/interfaces/index.ts
+++ src/auth/interfaces/index.ts
@@ -1,1 +1,2 @@
 export { JwtPayload } from './jwt-payload';
+export { ValidRoles } from './valid-roles';
