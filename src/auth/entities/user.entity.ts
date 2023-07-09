97df0ca8105441f548f5faa58cdb5dcfac6d474b
thezxzx <oscarmiguelayalaruelas@gmail.com>
2023-07-09 14:46:27 -0700

    Entidad del usuario

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,44 @@
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('varchar', {
+    unique: true,
+  })
+  email: string;
+
+  @Column('varchar', {
+    select: false,
+  })
+  password: string;
+
+  @Column('varchar', {
+    unique: true,
+  })
+  username: string;
+
+  @Column('varchar', {
+    array: true,
+    default: ['user'],
+  })
+  roles: string[];
+
+  @BeforeInsert()
+  checkFieldsBeforeInsert() {
+    this.email = this.email.toLowerCase().trim();
+  }
+
+  @BeforeUpdate()
+  checkFieldsBeforeUpdate() {
+    this.checkFieldsBeforeInsert();
+  }
+}
